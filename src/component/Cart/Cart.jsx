ce6ed077559465cc2dd5b6cb6306b59c57589ddd
Anuwar-Hosain <anuwarhossn@gmail.com>
2023-03-30 15:00:13 +0600

    cart section done

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -3,20 +3,26 @@ import "./Cart.css";
 
 const Cart = (props) => {
   const { cart } = props;
-  console.log(props);
+  console.log(cart);
   // total price section
   let total = 0;
+  let totalShipping = 0;
   for (const product of cart) {
     total = total + product.price;
+    totalShipping = totalShipping + product.shipping;
   }
+  // tax
+  let tax = (total * 7) / 100;
+  // grandTotal
+  let grandTotal = total + totalShipping + tax;
   return (
     <div className="cart">
       <h4 className="cart-title">Order Summary</h4>
       <p className="para">Selected Items: {cart.length}</p>
       <p className="para">Total Price: ${total}</p>
-      <p className="para">Total Shipping Charge: $5</p>
-      <p className="para">Tax: $114</p>
-      <p className="grand">Grand Total: $1559</p>
+      <p className="para">Total Shipping Charge: ${totalShipping}</p>
+      <p className="para">Tax: ${tax.toFixed(2)}</p>
+      <p className="grand">Grand Total: ${grandTotal.toFixed(2)}</p>
       <div>
         <button className="cart-btn">Clear Cart</button>
         <button className="review-btn">Review Order</button>
